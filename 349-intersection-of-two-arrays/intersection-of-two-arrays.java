c73040bbb82d95a53f57f841f11491b2a45539e9
Ann Zachariah <68020058+AnnZachariah@users.noreply.github.com>
2025-05-05 12:37:41 -0700

    Time: 3 ms (56.56%) | Memory: 43.7 MB (14.43%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,25 +1,27 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> intersect = new HashSet<>();
+       HashSet<Integer> set1 = new HashSet<>();
+       HashSet<Integer> set2 = new HashSet<>();
+       HashSet<Integer> intersect = new HashSet<>();
+       for(int i : nums1){
+        set1.add(i);
+       }
 
-        for(int i: nums1){
-            set1.add(i);
-        }
+       for(int i : nums2){
+        set2.add(i);
+       }
 
-        for(int i: nums2){
-            if(set1.contains(i)){
-                intersect.add(i);
-            }
+       for(int i : set1){
+        if(set2.contains(i)){
+            intersect.add(i);
         }
+       }
 
-        //create an array as we have to return array
-        int[] result = new int[intersect.size()];
-        int count = 0;
-        //for each loop to iterate and save into array
-        for(int i: intersect){
-            result[count++] = i;
-        }
-        return result;
+       int[] result = new int[intersect.size()];
+       int k = 0;
+       for(int i : intersect){
+        result[k++] = i ;
+       }
+       return result;
     }
 }
