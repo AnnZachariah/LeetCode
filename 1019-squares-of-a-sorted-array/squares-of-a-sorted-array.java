998910cb47d3ee948c76a8b03688e1658acfd3a0
Ann Zachariah <68020058+AnnZachariah@users.noreply.github.com>
2025-05-31 14:02:20 -0700

    Time: 7 ms (41.04%) | Memory: 47.5 MB (23.24%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] squared = new int[nums.length];
         for(int i = 0;i<nums.length;i++){
-            squared[i] = nums[i]*nums[i];
+            nums[i] = nums[i]*nums[i];
         }
-        Arrays.sort(squared);
-        return squared;
+        Arrays.sort(nums);
+        return nums;
     }
 }
