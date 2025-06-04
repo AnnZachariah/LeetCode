5796e8e07ce7d79d58586b1c5272b9ac907ea228
Ann Zachariah <68020058+AnnZachariah@users.noreply.github.com>
2025-06-04 13:07:45 -0700

    Time: 3 ms (79.17%) | Memory: 43.1 MB (74.37%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,22 +1,45 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        TreeSet<Integer> set = new TreeSet<>();
+        // TreeSet<Integer> set = new TreeSet<>();
 
-        for(int i : nums){
-            set.add(i);
-            if(set.size()>3){
-                set.remove(set.first());
+        // for(int i : nums){
+        //     set.add(i);
+        //     if(set.size()>3){
+        //         set.remove(set.first());
+        //     }
+        // }
+        // if(set.size() == 3){
+        //     return set.first();
+        // }
+        // if(set.size() < 3){
+        //     return set.last();
+        // }
+        // return set.last();
+
+        Long first = Long.MIN_VALUE;
+        Long second = Long.MIN_VALUE;
+        Long third = Long.MIN_VALUE;
+
+        for(int i: nums){
+            if(i==first || i==second || i==third){
+                continue;
+            }
+
+            if(i>first){
+                third = second;
+                second = first;
+                first = (long)i;
+            }
+            else if(i>second){
+                third = second;
+                second = (long)i;
+            }
+            else if(i>third){
+                third = (long)i;
             }
         }
 
-        if(set.size() == 3){
-            return set.first();
-        }
+        return third == Long.MIN_VALUE ? first.intValue() : third.intValue();
 
-        if(set.size() < 3){
-            return set.last();
-        }
-
-        return set.last();
     }
 }
